c0c64faf64c569bf73b5149baf9873f904d13eac
PietroLinguini <recklessraynard@gmail.com>
2023-03-05 00:40:44 -0500

    first commit :)))))))))))))))))))

---
+++ src/main/kotlin/com/github/crazyformccafe/bestea/BestTeaMod.kt
@@ -0,0 +1,9 @@
+package com.github.crazyformccafe.bestea
+import net.fabricmc.api.ModInitializer
+@Suppress("UNUSED")
+object BestTeaMod: ModInitializer {
+    private const val MOD_ID = "bestea"
+    override fun onInitialize() {
+        println("BesTea has been initialized.")
+    }
+}
